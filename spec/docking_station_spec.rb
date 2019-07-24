c54689c880473c817522b4f38a1a6599a184d047
EC2 Default User <ec2-user@ip-172-31-31-17.us-west-2.compute.internal>
2019-07-24 21:39:37 +0000

    Exercise 10

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,9 @@
 require 'DockingStation'
 describe DockingStation do
-  it {expect(DockingStation.new).to respond_to(:release_bike)}
+  it { expect(DockingStation.new).to respond_to(:release_bike) }
+  
+  it 'release a bike' do
+  bike = subject.release_bike
+  expect(bike).to be_working
+  end
 end
