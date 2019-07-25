4fbe87d91c4632c44ec002735da2f745068b3be2
Student <student@Makerss-MacBook-Pro.local>
2019-07-25 16:12:25 +0100

    Docking Bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,14 @@
 require 'DockingStation'
 describe DockingStation do
   it { expect(DockingStation.new).to respond_to(:release_bike) }
-  
   it 'release a bike' do
   bike = subject.release_bike
   expect(bike).to be_working
+  end
+  it { expect(DockingStation.new).to respond_to(:dock).with(1).argument }
+  it { expect(DockingStation.new).to respond_to(:bike) }
+  it 'dock a bike' do
+  bike = Bike.new
+  expect(subject.dock(bike)).to eq bike
   end
 end
